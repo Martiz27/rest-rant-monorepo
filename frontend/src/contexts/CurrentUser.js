7c2ec5a48c06a31e9cf796d9519f45423f4f551d
Charlie <ware.charlie914@gmail.com>
2023-01-05 20:23:45 -0800

    WIP

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -7,9 +7,12 @@ function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
-
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
